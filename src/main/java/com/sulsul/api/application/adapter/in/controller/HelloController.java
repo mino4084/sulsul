1fa1ae6f4989772c248ce9e06c0bdc3ff700fbb0
mino4084 <mino4084@kyobobook.com>
2023-07-04 17:32:13 +0900

    [공통][추가] helloController 테스트

---
+++ src/main/java/com/sulsul/api/application/adapter/in/controller/HelloController.java
@@ -0,0 +1,15 @@
+package com.sulsul.api.application.adapter.in.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/hello")
+public class HelloController {
+
+    @GetMapping
+    public String hello(){
+        return "hello";
+    }
+}
